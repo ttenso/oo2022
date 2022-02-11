7ab92194a58dc8e894116b6b641379067d650e25
ttenso <tenso@tlu.ee>
2022-02-11 15:10:52 +0200

    game switch-case, scanner, while, funktsioonid

--- 01_funktsioon/Game.java
+++ 01_funktsioon/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Game {
     // main -- pakub
     public static void main(String[] args) {
@@ -14,17 +16,63 @@ public class Game {
         // shot -- 32000kohta
         // Long --
         // booleaan
-        int wolrdHeight = 5;
+        int worldHeight = 5;
         int worldWidth = 10;
+        // math.random() -- 0..0.99999
+        int playerCoordinateY = generateRandomCoordinate(worldHeight);
+        int playerCoordinateX = generateRandomCoordinate(worldHeight);
+        int enemyCoordinateY = generateRandomCoordinate(worldHeight);
+        int enemyCoordinateX = generateRandomCoordinate(worldHeight);
+
+        printMap(worldHeight, worldWidth, playerCoordinateY, playerCoordinateX, enemyCoordinateY, enemyCoordinateX);
+
+        //klasside tegemine tund nr2
+        Scanner scanner = new Scanner(System.in);
+        String input =scanner.nextLine();
+
+
+        while (!input.equals("end")) {
+            //kontrollib ühte muutujat mingite väärtuste vastu
+            switch (input) {
+                case "a":
+                    playerCoordinateX--;
+                    break;
+                case "s":
+                    playerCoordinateX++;
+                    break;
+                case "d":
+                    playerCoordinateX++;
+                    break;
+                case "aw":
+                    playerCoordinateX--;
+                    break;
+            }
+            printMap(worldHeight, worldWidth, playerCoordinateY, playerCoordinateX, enemyCoordinateY, enemyCoordinateX);
+            input =scanner.nextLine();
+        }
+    }
+    // int -- sama tüüp mis peab olema funkt sees return järgi
+    public static int generateRandomCoordinate(int worldSize) {
+        return (int) (Math.random()*(worldSize-2))+1;
+    }
+
+    public static void printMap(int worldHeight, int worldWidth, int playerCoordinateY, int playerCoordinateX, int enemyCoordinateY, int enemyCoordinateX) {
+
         char symbol;               // deklareerib 1x muutuja
-        for (int y = 0; y < wolrdHeight; y++) {
+        for (int y = 0; y < worldHeight; y++) {
             for (int x = 0; x < worldWidth; x++) {
-                if (y == 0 || y == wolrdHeight-1) { // primitiiv|| AB päring
+                if (y == 0 || y == worldHeight-1) { // primitiiv|| AB päring
                     symbol = '-';
                 } else if (x == 0 || x == worldWidth-1) {
                     symbol = '|';
                 } else {
                     symbol = ' ';
+                }
+                if (playerCoordinateY == y && playerCoordinateX == x) {
+                    symbol = 'x';
+                }
+                if (enemyCoordinateY == y && enemyCoordinateX == x){
+                    symbol = 'z';
                 }
                 System.out.print(symbol);
             }
