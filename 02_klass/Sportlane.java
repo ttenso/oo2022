d34970f9f21a6b53eeb8cf86ac1a46effb84e0d6
tenso <tenso@tlu.ee>
2022-03-04 12:56:14 +0200

    kolmas_tund

---
+++ 02_klass/Sportlane.java
@@ -0,0 +1,34 @@
+public class Sportlane {
+        String eesnimi;
+        String perekonnanimi;
+        int vanus;
+        String sugu;
+        double Kaal;
+        double raskus;
+
+        public Sportlane(String eesnimi, String perekonnanimi, int vanus, String sugu, double Kaal, double raskus) {
+            this.eesnimi = eesnimi;
+            this.perekonnanimi = perekonnanimi;
+            this.vanus = vanus;
+            this.sugu = sugu;
+            this.Kaal = Kaal;
+            this.raskus = raskus;
+        }
+
+        public void keharaskus() {
+            System.out.println(eesnimi + perekonnanimi + " Surub rinnalt " + Math.round(raskus/Kaal) + " korda oma kehakaalu");
+        }
+
+        @Override
+        public String toString() {
+            return "Sportlane{" +
+                    "eesnimi='" + eesnimi + '\'' +
+                    ", perekonnanimi='" + perekonnanimi + '\'' +
+                    ", vanus=" + vanus +
+                    ", sugu=" + sugu +
+                    ", Kaal=" + Kaal +
+                    ", raskus=" + raskus +
+                    '}';
+        }
+    }
+
